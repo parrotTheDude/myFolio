65ccb0cf64c46c55f9cf80686c32442a4cbe6936
Jacob <program.acquaint533@passinbox.com>
2025-01-10 18:06:20 +1100

    Major work done to blog

--- blog.php
+++ blog.php
@@ -1,3 +1,20 @@
+<?php
+  // Import variables and functions for accessing db:
+  require_once('inc/variables.php');
+  require_once('inc/db-connect.php');
+
+  db_connect();
+
+  // SQL gets the number of records
+  $sql_statement = "SELECT postId FROM blog ORDER BY postId DESC LIMIT 1";
+  $blogs = $db_link->query($sql_statement) or die($db_link->error);
+  $blog_row = $blogs->fetch_assoc();
+
+  // Variable initalisation
+  $id = $blog_row['postId'];
+
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -16,8 +33,45 @@
 		</header>
 
 		<main>
-			<section>
-				
+			<section class="blogTestContainer">
+				<section class="blogTitleDiv">
+					<h1>welcome to my test blog</h1>
+					<p>this is just text to show how it might look when implimented onto a live website</p>
+				</section>
+				<!-- Displays all the panels -->
+				<section class="blogDisplayPanel">
+
+					<?php
+						// SQL to fetch the events
+		        $sql_statement = "
+		        SELECT title, description, link, image, author, dateCreated
+		        FROM blog 
+		        ORDER BY postId ASC";
+
+		        $users = $db_link->query($sql_statement) or die($db_link->error); 
+
+						
+	          while($user_row = $users->fetch_assoc()) {
+        	?>
+
+					<!-- Blog Panel -->
+					<section class="blogPanel">
+						<a class="blogLink" href="https://jbowerman.com/<?php echo $user_row['link'] ?>">
+							<section class="blogImgContainer">
+								<img src="img/<?php echo $user_row['image']; ?>">
+							</section>
+							<section class="blogText">
+								<h2><?php echo $user_row['title']; ?></h2>
+								<section class="blogDetails">
+									<p>By <?php echo $user_row['author']; ?> | <?php echo $user_row['dateCreated']; ?></p>
+								</section>
+								<p><?php echo $user_row['description']; ?></p>
+							</section>
+						</a>
+					</section>
+					 <?php } ?>
+
+				</section>
 			</section>
 		</main>
 
---
+++ inc/db-connect.php
@@ -0,0 +1,19 @@
+<?php
+  
+function db_connect($server = DB_SERVER, $username = DB_SERVER_USERNAME, $password = DB_SERVER_PASSWORD, $database = DB_DATABASE, $link = 'db_link') {
+  
+  global $$link;
+  
+  $$link = new mysqli($server, $username, $password, $database);
+  
+  if ($$link) mysqli_select_db($$link, $database) or die("Error Connecting To Database");
+  return $$link;
+  
+}
+  
+function db_close($link = 'db_link') {
+  
+  global $$link;
+  return mysql_close($$link);
+  
+}
