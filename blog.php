5bde272629632bbd19655b4b50ab9b17d5a01ff3
Jacob <program.acquaint533@passinbox.com>
2025-01-10 15:42:17 +1100

    Set up the base page for my blog testing site

---
+++ blog.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php
+	    $page_title = 'Full Stack Developer | Jacob Bowerman';
+	    $page_decription = "Welcome to my website! This is the place I keep all my projects and some fun things too!";
+	    $page_name = '/';
+	    include ('inc/head.php');
+	    include ('inc/schema.php');
+  	?>
+	</head>
+	
+	<body>
+		<header>
+			
+		</header>
+
+		<main>
+			<section>
+				
+			</section>
+		</main>
+
+		<footer>
+
+		</footer>
+
+	</body>
+</html>
